a59d10a5314b8ce3cc15bb72d0c5db36f7881bae
2025010019 <108616525+2025010019@users.noreply.github.com>
2026-04-20 15:59:01 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n=0, count_num=0, count_alpha=0;
+    int num_max=0, alpha_max=0;
+    char ch='\0';
+    
+    int i;
+    
+    scanf("%d", &n);
+    
+    for (i=1; i<=n; i++)
+    {
+        scanf("%c", &ch);
+        if(ch>='a' && ch<='z'){
+            count_alpha++;
+            count_num=0;
+            
+            if(alpha_max<=count_alpha) alpha_max=count_alpha;
+        }
+        else if(ch>='0' && ch<='9'){
+            count_num++;
+            count_alpha=0;
+            
+            if(num_max<=count_num) num_max=count_num;
+        }
+        else{
+        }
+    }
+    
+    printf("%d\n%d", alpha_max, num_max);
+    
+    
+    return 0;
+}
